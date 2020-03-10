dcf92508113056ed72743f17eaf869a96d601a1d
Rocky90833 <452527164@qq.com>
2020-03-10 22:04:13 +0800

    test

---
+++ src/main/java/com/atguigu/springboot/type/SysConfigKey.java
@@ -0,0 +1,14 @@
+package com.atguigu.springboot.type;
+
+public interface SysConfigKey {
+
+    /**
+     * 操作日志开关
+     */
+    String OPERATE_LOG_SWITCH = "OPERATE_LOG_SWITCH";
+
+    /**
+     * 测试开关
+     */
+    String Test_SWITCH = "Test_SWITCH";
+}
