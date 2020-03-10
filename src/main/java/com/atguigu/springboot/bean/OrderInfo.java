15121ea71dfbcc182cb2f1c7082ca7cd8c18ee2b
Rocky90833 <452527164@qq.com>
2020-03-10 22:00:31 +0800

    test

--- src/main/java/com/atguigu/springboot/bean/OrderInfo.java
+++ src/main/java/com/atguigu/springboot/bean/OrderInfo.java
@@ -1,9 +1,15 @@
 package com.atguigu.springboot.bean;
 
-import com.atguigu.springboot.enums.OrderInfoEnum;
+import com.atguigu.springboot.type.enums.OrderInfoEnum;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
+@NoArgsConstructor  //添加无参构造方法
+@AllArgsConstructor //添加全参构方法
+@Accessors(chain = true)  //添加链式调用
 public class OrderInfo {
 
     private Integer id;
