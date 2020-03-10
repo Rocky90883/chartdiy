619fceabe2cb42674e43c2e57e697ebf2d91a7a4
Rocky90833 <452527164@qq.com>
2020-03-10 22:19:33 +0800

    test

---
+++ src/main/java/com/atguigu/springboot/utils/LoginCacheUtil.java
@@ -0,0 +1,12 @@
+package com.atguigu.springboot.utils;
+
+import com.atguigu.springboot.my.bean.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginCacheUtil {
+
+    public static Map<String,User> loginuser = new HashMap<>();
+
+}
